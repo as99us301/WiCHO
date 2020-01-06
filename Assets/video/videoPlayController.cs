0bd3642ba17e4c29ed03afc674da93e7a524fc64
as99us301 <as99us301@gmail.com>
2020-01-06 18:03:20 +0800

    加入海報辨識影片

---
+++ Assets/video/videoPlayController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class videoPlayController : MonoBehaviour
+{
+    public VideoPlayer vp;
+    public Texture defaultTexture;
+    public RenderTexture renderTexture;
+
+    private void OnEnabled() 
+    {
+        if (vp != null) vp.Play();
+    }
+
+    private void OnDisabled()
+    {
+        if (vp != null) {
+            vp.Stop();
+            Graphics.Blit(defaultTexture, renderTexture);
+        
+        }
+    }
+}
