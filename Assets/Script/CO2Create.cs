68acfb357ad9356dfc35bc649074fc9745ad5fa5
as99us301 <as99us301@gmail.com>
2020-01-07 02:24:59 +0800

    加入CO2模型+改寫碰撞後隱藏元素方法

--- Assets/Script/CO2Create.cs
+++ Assets/Script/CO2Create.cs
@@ -2,29 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class H2OCreate : MonoBehaviour {
+public class CO2Create : MonoBehaviour
+{
+    
     public GameObject Newthing;
     public GameObject Newthing2;
     public GameObject Newthing3;
-    public GameObject furnace;
     public GameObject introd; //化合物簡介
-    public GameObject ice;
     public GameObject Instantiate_Pos1;
     public GameObject Instantiate_Pos2;
     public GameObject patentsPrefeb;
-    public GameObject ButtonCanvas;
-    public GameObject Botton1;
-    public GameObject Botton2;
-    public GameObject Botton3;
-    public GameObject Botton4;
-    public GameObject Botton5;
     private bool ColWith1 = false;
     private bool ColWith2 = false;
 
-    public GameObject Hcanvas, Ocanvas;
+
     public GameObject[] ElementArray;
 
+    public GameObject Ccanvas, Ocanvas;
+
     private GameObject checkImage;
+
     void Start()
     {
         checkImage = GameObject.Find("checkImage");
@@ -34,23 +31,25 @@ public class H2OCreate : MonoBehaviour {
 
     void OnCollisionEnter(Collision collision) //當碰撞開始後
     {
-        if (collision.gameObject.tag == "H")
+        if (collision.gameObject.tag == "C")
         {
             ColWith1 = true;
-        } else if (collision.gameObject.layer == LayerMask.NameToLayer("Num3")) {
+        }
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Num3"))
+        {
             ColWith2 = true;
         }
 
         if (ColWith1 && ColWith2)
         {
-            CloseCanvas();
+            checkImage.SetActive(false);
             for (int i = 0; i < ElementArray.Length; i++)
             {
                 ElementArray[i].gameObject.SetActive(false);
             }
-            checkImage.SetActive(false);
-            GameObject H2O = Instantiate(Newthing, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-            H2O.transform.parent = patentsPrefeb.transform;
+            CloseCanvas();
+            GameObject CO2 = Instantiate(Newthing, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
+            CO2.transform.parent = patentsPrefeb.transform;
         }
 
     }
@@ -59,59 +58,60 @@ public class H2OCreate : MonoBehaviour {
     {
         if (ColWith1 && ColWith2)
         {
-            ButtonCanvas.SetActive(true);
+           /*ButtonCanvas.SetActive(true);
             Botton1.SetActive(true);
             Botton2.SetActive(true);
             Botton3.SetActive(true);
             Botton4.SetActive(true);
-            Botton5.SetActive(true);
+            Botton5.SetActive(true);*/
         }
     }
 
     void OnCollisionExit(Collision collision) //當碰撞結束後
     {
-        if (collision.gameObject.tag == "H")
+        if (collision.gameObject.tag == "C")
         {
             ColWith1 = false;
-            ButtonCanvas.SetActive(false);
-            Botton1.SetActive(false);
-            Botton2.SetActive(false);
-            Botton3.SetActive(false);
-            Botton4.SetActive(false);
-            Botton5.SetActive(false);
-            CleanObj();
             for (int i = 0; i < ElementArray.Length; i++)
             {
                 ElementArray[i].gameObject.SetActive(true);
             }
+            /*ButtonCanvas.SetActive(false);
+            Botton1.SetActive(false);
+            Botton2.SetActive(false);
+            Botton3.SetActive(false);
+            Botton4.SetActive(false);
+            Botton5.SetActive(false); */
+            CleanObj(); 
         }
         else if (collision.gameObject.layer == LayerMask.NameToLayer("Num3"))
         {
             ColWith2 = false;
-            ButtonCanvas.SetActive(false);
-            Botton1.SetActive(false);
-            Botton2.SetActive(false);
-            Botton3.SetActive(false);
-            Botton4.SetActive(false);
-            Botton5.SetActive(false);
-            CleanObj();
             for (int i = 0; i < ElementArray.Length; i++)
             {
                 ElementArray[i].gameObject.SetActive(true);
             }
+            /*ButtonCanvas.SetActive(false);
+            Botton1.SetActive(false);
+            Botton2.SetActive(false);
+            Botton3.SetActive(false);
+            Botton4.SetActive(false);
+            Botton5.SetActive(false); */
+            CleanObj();
         }
     }
 
     public void button1Click() //分子結構按鈕
     {
-        CleanObj();
-        introd.SetActive(false);
-        GameObject H2O = Instantiate(Newthing, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-        H2O.transform.parent = patentsPrefeb.transform;
+        //CleanObj();
+        //introd.SetActive(false);
+        GameObject CO2 = Instantiate(Newthing, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
+        CO2.transform.parent = patentsPrefeb.transform;
     }
 
+    /*
     public void button2Click() //液體按鈕
-    {      
+    {
         CleanObj();
         introd.SetActive(false);
         GameObject H2O2 = Instantiate(Newthing2, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
@@ -135,26 +135,20 @@ public class H2OCreate : MonoBehaviour {
         GameObject icee = Instantiate(ice, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
         icee.transform.parent = patentsPrefeb.transform;
     }
-    
+
     public void button5Click() //簡介按鈕
     {
         CleanObj();
         introd.SetActive(true);
-    }
+    }*/
 
     public void CleanObj() //清理生成出來的物件
-    {
-        Destroy(GameObject.Find("water(Clone)"));
-        Destroy(GameObject.Find("H2O_Prefeb(Clone)"));
-        Destroy(GameObject.Find("kettle(Clone)"));
-        Destroy(GameObject.Find("fire(Clone)"));
-        Destroy(GameObject.Find("furnace(Clone)"));
-        Destroy(GameObject.Find("ice(Clone)"));
+    {        
+        Destroy(GameObject.Find("CO2_Prefeb(Clone)"));
     }
-
     public void CloseCanvas()
     {
-        Hcanvas.SetActive(false);
+        Ccanvas.SetActive(false);
         Ocanvas.SetActive(false);
-    }
+    } 
 }
