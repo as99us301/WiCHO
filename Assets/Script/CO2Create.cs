ade7af9a3d30b5ecef42fd169f554faa639248c4
as99us301 <as99us301@gmail.com>
2020-01-07 16:58:03 +0800

    調整co2化合物 + 程式

--- Assets/Script/CO2Create.cs
+++ Assets/Script/CO2Create.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class CO2Create : MonoBehaviour
 {
-    
     public GameObject Newthing;
-    public GameObject Newthing2;
-    public GameObject Newthing3;
-    public GameObject introd; //化合物簡介
     public GameObject Instantiate_Pos1;
     public GameObject Instantiate_Pos2;
     public GameObject patentsPrefeb;
@@ -17,9 +13,7 @@ public class CO2Create : MonoBehaviour
 
 
     public GameObject[] ElementArray;
-
     public GameObject Ccanvas, Ocanvas;
-
     private GameObject checkImage;
 
     void Start()
@@ -31,11 +25,11 @@ public class CO2Create : MonoBehaviour
 
     void OnCollisionEnter(Collision collision) //當碰撞開始後
     {
-        if (collision.gameObject.tag == "C")
+        if (collision.gameObject.tag == "O")
         {
             ColWith1 = true;
         }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("Num3"))
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Num4"))
         {
             ColWith2 = true;
         }
@@ -58,12 +52,8 @@ public class CO2Create : MonoBehaviour
     {
         if (ColWith1 && ColWith2)
         {
-           /*ButtonCanvas.SetActive(true);
-            Botton1.SetActive(true);
-            Botton2.SetActive(true);
-            Botton3.SetActive(true);
-            Botton4.SetActive(true);
-            Botton5.SetActive(true);*/
+            checkImage.SetActive(false);
+            return;
         }
     }
 
@@ -76,71 +66,18 @@ public class CO2Create : MonoBehaviour
             {
                 ElementArray[i].gameObject.SetActive(true);
             }
-            /*ButtonCanvas.SetActive(false);
-            Botton1.SetActive(false);
-            Botton2.SetActive(false);
-            Botton3.SetActive(false);
-            Botton4.SetActive(false);
-            Botton5.SetActive(false); */
-            CleanObj(); 
+            Destroy(GameObject.Find("CO2_Prefeb(Clone)"));
         }
-        else if (collision.gameObject.layer == LayerMask.NameToLayer("Num3"))
+        else if (collision.gameObject.layer == LayerMask.NameToLayer("Num4"))
         {
             ColWith2 = false;
             for (int i = 0; i < ElementArray.Length; i++)
             {
                 ElementArray[i].gameObject.SetActive(true);
             }
-            /*ButtonCanvas.SetActive(false);
-            Botton1.SetActive(false);
-            Botton2.SetActive(false);
-            Botton3.SetActive(false);
-            Botton4.SetActive(false);
-            Botton5.SetActive(false); */
-            CleanObj();
+            Destroy(GameObject.Find("CO2_Prefeb(Clone)"));
         }
     }
-
-    public void button1Click() //分子結構按鈕
-    {
-        //CleanObj();
-        //introd.SetActive(false);
-        GameObject CO2 = Instantiate(Newthing, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-        CO2.transform.parent = patentsPrefeb.transform;
-    }
-
-    /*
-    public void button2Click() //液體按鈕
-    {
-        CleanObj();
-        introd.SetActive(false);
-        GameObject H2O2 = Instantiate(Newthing2, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-        H2O2.transform.parent = patentsPrefeb.transform;
-    }
-
-    public void button3Click() //氣體按鈕
-    {
-        CleanObj();
-        introd.SetActive(false);
-        GameObject kettle = Instantiate(Newthing3, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-        kettle.transform.parent = patentsPrefeb.transform;
-        GameObject furnace2 = Instantiate(furnace, Instantiate_Pos2.transform.position, Instantiate_Pos2.transform.rotation);
-        furnace2.transform.parent = patentsPrefeb.transform;
-    }
-
-    public void button4Click() //固體按鈕
-    {
-        CleanObj();
-        introd.SetActive(false);
-        GameObject icee = Instantiate(ice, Instantiate_Pos1.transform.position, Instantiate_Pos1.transform.rotation);
-        icee.transform.parent = patentsPrefeb.transform;
-    }
-
-    public void button5Click() //簡介按鈕
-    {
-        CleanObj();
-        introd.SetActive(true);
-    }*/
 
     public void CleanObj() //清理生成出來的物件
     {        
