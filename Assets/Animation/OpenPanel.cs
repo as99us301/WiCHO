9166fe565769b42ff6a07c72bbb7416d9c4027e4
as99us301 <as99us301@gmail.com>
2020-03-07 23:05:11 +0800

    測試點擊按鈕讓panel上滑與下滑 測試成功，還未更新

---
+++ Assets/Animation/OpenPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenPanel : MonoBehaviour
+{
+    public GameObject Menupanel;
+    private Animator animator;
+    private bool isOpen;
+
+    public void Start()
+    {
+        animator = Menupanel.GetComponent<Animator>();
+        isOpen = animator.GetBool("Open");
+    }
+
+    public void Open() {
+        if (Menupanel != null) {
+
+            if (animator != null) {
+
+                if (!isOpen) {
+                    UpMenu();
+                }else{
+                    DownMenu();
+                }
+            }
+        }
+    }
+
+    private void UpMenu() 
+    {
+        isOpen = true;
+        animator.SetBool("Open", isOpen);
+    }
+
+    private void DownMenu() 
+    {
+        isOpen = false;
+        animator.SetBool("Open", isOpen);
+    }
+}
