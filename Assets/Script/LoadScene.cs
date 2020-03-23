082001ed35ec5e5350ba26c243118f4c1cb1f1a9
as99us301 <as99us301@gmail.com>
2020-03-23 23:27:36 +0800

    更新拼圖辨識圖+整理檔案

---
+++ Assets/Script/LoadScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void GallaryOnClick()
+    {
+        SceneManager.LoadScene("Element");
+        //Application.LoadLevel(4);
+    }
+    public void AROnClick()
+    {
+        SceneManager.LoadScene("AR_index");
+        //Application.LoadLevel(1);
+    }
+    public void LabOnClick()
+    {
+        SceneManager.LoadScene("Lab");
+        //Application.LoadLevel(0);
+    }
+
+    public void GameOnClick()
+    {
+        SceneManager.LoadScene("NewGame");
+        //Application.LoadLevel(2);
+    }
+    public void ExbisionOnClick()
+    {
+        SceneManager.LoadScene("Exhibition");
+        //Application.LoadLevel(3);
+    }
+    public void backtoIndex()
+    {
+        SceneManager.LoadScene("index");
+        //Application.LoadLevel(0);
+    }
+}
