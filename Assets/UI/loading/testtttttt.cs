64e238fe4f7d815d76b665abde9da56556e4223a
as99us301 <as99us301@gmail.com>
2020-03-24 16:01:08 +0800

    新增化合物模型+檔案整理

---
+++ Assets/UI/loading/testtttttt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testtttttt : MonoBehaviour
+{
+    public Animation animation;
+    void Start()
+    {
+        this.GetComponent<Animation>().Play("FakeLoading");
+    }
+
+}
